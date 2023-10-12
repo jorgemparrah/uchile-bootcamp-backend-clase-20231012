0d7d3f2595669596ebaaa9e3e2a917abe0160010
Jorge Parra <jorge.parra@karibu.cl>
2023-10-12 19:55:52 -0300

    Conexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { DescuentoModule } from './descuento/descuento.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [DescuentoModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: "root",
+      password: "clave123",
+      database: 'EJEMPLO_PEDIDOS'
+    }),
+    DescuentoModule
+  ],
   controllers: [],
   providers: [],
 })
